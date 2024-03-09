ee51098823efd084d2d5aef819665823ae6ef876
geetarajagopal <geeta.rajagopal@gmail.com>
2024-03-09 18:22:48 -0500

    First commit

---
+++ lebronjahames.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+import Adafruit_DHT
+import time
+import requests
+from datetime import datetime
+import os
+import csv
+import shutil
+import requests
+import json
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+def multiple(m,n):
+    return True if m % n == 0 else False
+
+lat = 40.600592
+lon = -74.343371
+part = 'alerts'
+API_Key = '2bb600d0a46e09995cd0d2ab71e1a3b6'
+#API_Key = 'Key1'
+#'f54b400919e97f5cf3c475301f4b396d'
+api_url = 'https://api.openweathermap.org/data/3.0/onecall?lat='+str(lat)+'&lon='+str(lon)+'&exclude='+part+'&appid='+API_Key
+#response = requests.get(api_url).json()
+#response_dict = json.loads(str(response.json()))
+print("Program called")
+filename = '/home/pi/Desktop/data.csv'
+getValues = lambda key,inputData: [subVal[key] if key in subVal else 0 for subVal in inputData]
+file_size = os.stat(filename).st_size/(1024*1024)
+if file_size >= 1:
+    source = '/home/pi/Desktop/data.csv'
+    archive_fileName = 'data_'+str(datetime.now())
+    dest = '/home/pi/Desktop/Archive/'+archive_fileName
+    shutil.move(source,dest)
+    f = open(filename, 'x')
+with open(filename, 'a', newline='') as csvfile:
+    i = 0
+    while i <= 5:
+
+        csv_writer = csv.writer(csvfile)
+    #        if(multiple(time.localtime().tm_min,1)):
+    #    current_time = datetime.now()
+
+
+        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+
+        if humidity is not None and temperature is not None:
+            print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
+            try:
+                response  = requests.get(api_url).json()
+                #print(response)
+                #print(response['current']['temp'],response['current']['humidity'])
+                #print(getValues('snow',response['hourly']))
+                #print(response['hourly'][0]['rain']['1h'])
+                if abs(temperature - (response['current']['temp']-273.15)) >=5:
+                    location_value = "indoor"
+                else:
+                    location_value = "outdoor"
+                csv_writer.writerow([datetime.now(),humidity,temperature,response['current']['temp'],response['current']['humidity'],getValues('temp',response['hourly']),getValues('humidity',response['hourly']),location_value,getValues('rain',response['hourly']),getValues('snow',response['hourly'])])
+                print("appended")
+                break
+            except Exception as e:
+                print("exception")
+                time.sleep(1)
+        else:
+            print("are you sped")
+            i = i+1
+            time.sleep(1);
+     #       else:
+     #           print("not the right time")
