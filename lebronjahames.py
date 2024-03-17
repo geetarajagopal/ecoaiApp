fafc7e3fa628a4ec0a95a99ae6599f701b01c7c0
geetarajagopal <89715112+geetarajagopal@users.noreply.github.com>
2024-03-17 12:42:26 -0400

    Update lebronjahames.py

--- lebronjahames.py
+++ lebronjahames.py
@@ -8,6 +8,10 @@ import csv
 import shutil
 import requests
 import json
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
@@ -18,6 +22,17 @@ lat = 40.600592
 lon = -74.343371
 part = 'alerts'
 API_Key = '2bb600d0a46e09995cd0d2ab71e1a3b6'
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
+
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
+
+# Create differential input between channel 0 and 1
+#chan = AnalogIn(ads, ADS.P0, ADS.P1)
 #API_Key = 'Key1'
 #'f54b400919e97f5cf3c475301f4b396d'
 api_url = 'https://api.openweathermap.org/data/3.0/onecall?lat='+str(lat)+'&lon='+str(lon)+'&exclude='+part+'&appid='+API_Key
@@ -41,9 +56,11 @@ with open(filename, 'a', newline='') as csvfile:
     #        if(multiple(time.localtime().tm_min,1)):
     #    current_time = datetime.now()
 
-
-        humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-
+        min_vol = 0.92
+        max_vol = 2.45
+        humidity_old, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+        volt_val = round(chan.voltage,2)
+        humidity = ((1-(volt_val-min_vol))/(max_vol-min_vol))*100
         if humidity is not None and temperature is not None:
             print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
             try:
@@ -56,7 +73,7 @@ with open(filename, 'a', newline='') as csvfile:
                     location_value = "indoor"
                 else:
                     location_value = "outdoor"
-                csv_writer.writerow([datetime.now(),round(humidity,2),round(temperature,2),response['current']['temp'],response['current']['humidity'],getValues('temp',response['hourly']),getValues('humidity',response['hourly']),location_value,getValues('rain',response['hourly']),getValues('snow',response['hourly'])])
+                csv_writer.writerow([datetime.now(),round(humidity,2),volt_val,round(temperature,2),response['current']['temp'],response['current']['humidity'],getValues('temp',response['hourly']),getValues('humidity',response['hourly']),location_value,getValues('rain',response['hourly']),getValues('snow',response['hourly'])])
                 print("appended")
                 break
             except Exception as e:
