564902dce16b6c3da8ed85a27f59164d8f89addc
geetarajagopal <89715112+geetarajagopal@users.noreply.github.com>
2024-03-24 08:42:23 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -569,7 +569,7 @@ def serve_layout():
     df_withforecast["Date"] = pd.to_datetime(df_withforecast["Date"].astype(str),format="mixed")
     df_withforecast.head(5)
     df_withforecast.columns
-    if ARIMA_24hrs_pred >= 0.7:
+    if ARIMA_24hrs_pred >= 70:
         msg_to_display = "You do not need to water in next 24 hours"
     else:
         msg_to_display = "You will need to water in next 24 hours"
@@ -647,7 +647,7 @@ def serve_layout():
     # ),
       
     html.H1(children='Eco-AI Data', style={'textAlign':'center'}),
-    html.Meta(httpEquiv='refresh',content="300"),
+    html.Meta(httpEquiv='refresh',content="30000"),
     html.H3(msg_to_display),
     html.H3("Predicted Humidity for next 24 hours by different models:"),
     dash_table.DataTable(data=df_results_prediction.to_dict('records'),page_size=10),
